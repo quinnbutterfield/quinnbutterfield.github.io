4593fb719f9b0fce2e835d025b7453b8102b67d4
Quinn <quinnob66@gmail.com>
2020-08-19 22:02:44 -0700

    Updates

---
+++ precache-manifest.f2debc0fe1390c82fbdf5f5d8e99b47b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebb77458d0599b2818940b23dbc0779",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aba2b0b397c10b585393",
+    "url": "/static/css/main.8d216c0f.chunk.css"
+  },
+  {
+    "revision": "1b762e4c7e860292e69e",
+    "url": "/static/js/2.6e7d58c1.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.6e7d58c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba2b0b397c10b585393",
+    "url": "/static/js/main.11853c93.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
